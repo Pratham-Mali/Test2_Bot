123243d36ad25e259ae52107354a228689a406fb
Pratham-Mali <115096495+Pratham-Mali@users.noreply.github.com>
2022-10-05 20:48:29 +0530

    Add files via upload

---
+++ Test2_Bot/responses.py
@@ -0,0 +1,13 @@
+
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if p_message == 'hello':
+        return 'Hey there!'
+
+
+    if p_message == '!help':
+        return "`This is a help message that you can modify.`"
+
+    #  return 'Yeah, I don\'t know. Try typing "!help".'
